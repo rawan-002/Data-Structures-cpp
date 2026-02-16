19c537e1cb2cfbe9e1e6a3029fb102916aeb92d2
Rawan-001 <ra20awn@gmail.com>
2026-02-16 12:46:29 +0300

    Add full MyStack project with .gitignore

---
+++ source/repos/MyStack/MyStack/Queue.h
@@ -0,0 +1,87 @@
+#pragma once
+#include <iostream>
+#include"clsDblLinkedList.h"
+using namespace std;
+
+template <class T>
+
+class clsQueue
+{
+
+protected:
+	clsDblLinkedList<T> _MyList;
+
+public:
+
+
+	void push(T Item)
+	{
+		_MyList.InsertAtEnd(Item);
+	}
+
+	void pop()
+	{
+		_MyList.DeleteFirstNode();
+	}
+
+	void Print()
+	{
+		_MyList.PrintList();
+	}
+
+	int Size()
+	{
+		return _MyList.Size();
+	}
+
+	bool isEmpty()
+	{
+		return _MyList.IsEmpty();
+	}
+
+	T front()
+	{
+		return _MyList.GetItem(0);
+	}
+
+	T back()
+	{
+		return _MyList.GetItem(Size() - 1);
+	}
+	/***********************************************************/
+
+	T GetItem(int index)
+	{
+		return _MyList.GetItem(index);
+	}
+
+	void Reverse()
+	{
+		_MyList.Reverse();
+	}
+
+	bool UpdateItem(int Index, T NewValue)
+	{
+		return _MyList.UpdateItem(Index, NewValue);
+	}
+
+	void InsertAfter(int Index, T NewValue)
+	{
+		_MyList.InsertAfter(Index, NewValue);
+	}
+
+	void InsertAtFront(T Value)
+	{
+		_MyList.InsertAtBeginning(Value);
+	}
+
+	void InsertAtBack(T Value)
+	{
+		_MyList.InsertAtEnd(Value);
+	}
+
+	void Clear()
+	{
+		_MyList.Clear();
+	}
+};
